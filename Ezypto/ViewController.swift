a0de50c0e46b1729ad2e600c3b6d8bb563437b15
shane.chi <yuhsuan19@gmail.com>
2025-02-28 23:34:22 +0800

    create xcode project

---
+++ Ezypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ezypto
+//
+//  Created by Shane Chi on 2025/2/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
